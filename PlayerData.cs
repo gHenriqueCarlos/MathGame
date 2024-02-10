a4c0f0762a70f58c82345e4d7e0b36e5f04f35c6
Carlos Henrique <45677964+gHenriqueCarlos@users.noreply.github.com>
2024-02-09 21:35:33 -0300

    Player data

--- PlayerData.cs
+++ PlayerData.cs
@@ -2,12 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace MathGame
 {
+    public class PlayerDataEx
+    {
+        
+    }
     public class PlayerData
     {
-        public int 
+        public int WrongAnswers = 0;
+        public int RightAnswers = 0;
+        public int Level = 0;
+
+        public PlayerData(bool loadPlayerData) { 
+            if(loadPlayerData){
+                LoadPlayerData();
+            }
+        }
+
+        public void LoadPlayerData(){
+            WrongAnswers = 0;
+            RightAnswers = 0;
+            Level = 0;
+
+            // Load Data
+        }
+        public void SavePlayerData(int WrongAnswers, int RightAnswers, int Level){
+            // Save Data
+        }
     }
 }
